635d72b72f98e7475061012da494b94709c24b4d
Chris Cameron <chris.cameron@wedoresi.com>
2017-09-01 12:54:47 -0400

    Update IO port state when assigned

--- ICD.Connect.Misc.CrestronPro/Ports/IoPort/IoPortAdapter.cs
+++ ICD.Connect.Misc.CrestronPro/Ports/IoPort/IoPortAdapter.cs
@@ -69,6 +69,11 @@ namespace ICD.Connect.Misc.CrestronPro.Ports.IoPort
 			Subscribe(m_Port);
 
 			UpdateCachedOnlineStatus();
+
+			DigitalIn = m_Port != null && m_Port.DigitalIn;
+			DigitalOut = m_Port != null && m_Port.DigitalOut;
+			AnalogIn = m_Port == null ? (ushort)0 : m_Port.AnalogIn;
+			Configuration = m_Port == null ? eIoPortConfiguration.None : s_ConfigMap.GetKey(m_Port.VersiportConfiguration);
 		}
 
 		/// <summary>
@@ -141,6 +146,12 @@ namespace ICD.Connect.Misc.CrestronPro.Ports.IoPort
 		/// <param name="digitalOut"></param>
 		public override void SetDigitalOut(bool digitalOut)
 		{
+			if (m_Port == null)
+			{
+				Logger.AddEntry(eSeverity.Error, "{0} failed to set digital out - no port assigned", this);
+				return;
+			}
+
 			if (m_Port.VersiportConfiguration != eVersiportConfiguration.DigitalOutput)
 			{
 				Logger.AddEntry(eSeverity.Error, "{0} failed to set digital out - not configured as a digital output", this);
@@ -245,7 +256,6 @@ namespace ICD.Connect.Misc.CrestronPro.Ports.IoPort
 #if SIMPLSHARP
             SetIoPort(null, 0);
 #endif
-			Configuration = eIoPortConfiguration.None;
 		}
 
 		/// <summary>
