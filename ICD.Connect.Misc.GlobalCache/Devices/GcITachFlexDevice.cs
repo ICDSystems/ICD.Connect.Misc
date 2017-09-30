f255c6983632916e11af8879e7d07a87fdd48f2e
Chris Cameron <chris.cameron@wedoresi.com>
2017-09-30 14:59:12 -0400

    Renaming flex device, settings take an Address property

---
+++ ICD.Connect.Misc.GlobalCache/Devices/GcITachFlexDevice.cs
@@ -0,0 +1,14 @@
+﻿using ICD.Connect.Devices;
+
+namespace ICD.Connect.Misc.GlobalCache.Devices
+{
+    public sealed class GcITachFlexDevice : AbstractDevice<GcITachFlexDeviceSettings>
+	{
+		private const ushort TCP_PORT = 4998;
+
+		protected override bool GetIsOnlineStatus()
+		{
+			return false;
+		}
+	}
+}
---
+++ ICD.Connect.Misc.GlobalCache/Devices/GcITachFlexDeviceSettings.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ICD.Common.Properties;
+using ICD.Connect.Devices;
+using ICD.Connect.Settings.Attributes;
+using ICD.Common.Utils.Xml;
+
+namespace ICD.Connect.Misc.GlobalCache.Devices
+{
+    public sealed class GcITachFlexDeviceSettings : AbstractDeviceSettings
+	{
+		private const string FACTORY_NAME = "iTachFlex";
+
+		private const string ADDRESS_ELEMENT = "Address";
+
+		public override string FactoryName { get { return FACTORY_NAME; } }
+
+		public override Type OriginatorType { get { return typeof(GcITachFlexDevice); } }
+
+		/// <summary>
+		/// The network address of the iTach device.
+		/// </summary>
+		public string Address { get; set; }
+
+		/// <summary>
+		/// Writes property elements to xml.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void WriteElements(IcdXmlTextWriter writer)
+		{
+			base.WriteElements(writer);
+
+			writer.WriteElementString(ADDRESS_ELEMENT, Address);
+		}
+
+		/// <summary>
+		/// Loads the settings from XML.
+		/// </summary>
+		/// <param name="xml"></param>
+		/// <returns></returns>
+		[PublicAPI, XmlFactoryMethod(FACTORY_NAME)]
+		public static GcITachFlexDeviceSettings FromXml(string xml)
+		{
+			GcITachFlexDeviceSettings output = new GcITachFlexDeviceSettings
+			{
+				Address = XmlUtils.TryReadChildElementContentAsString(xml, ADDRESS_ELEMENT)
+			};
+
+			ParseXml(output, xml);
+			return output;
+		}
+	}
+}
