1046d17dcb57ca3932f13a8d2918fcfc5997cea2
Chris Cameron <chris.cameron@wedoresi.com>
2017-09-29 21:48:57 -0400

    Stub classes for device instantiation

---
+++ ICD.Connect.Misc.GlobalCache/GlobalCacheFlexDevice.cs
@@ -0,0 +1,12 @@
+﻿using ICD.Connect.Devices;
+
+namespace ICD.Connect.Misc.GlobalCache
+{
+    public sealed class GlobalCacheFlexDevice : AbstractDevice<GlobalCacheFlexDeviceSettings>
+    {
+		protected override bool GetIsOnlineStatus()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ ICD.Connect.Misc.GlobalCache/GlobalCacheFlexDeviceSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ICD.Common.Properties;
+using ICD.Connect.Devices;
+using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Misc.GlobalCache
+{
+    public sealed class GlobalCacheFlexDeviceSettings : AbstractDeviceSettings
+	{
+		private const string FACTORY_NAME = "GlobalCacheFlex";
+
+		public override string FactoryName { get { return FACTORY_NAME; } }
+
+		public override Type OriginatorType { get { return typeof(GlobalCacheFlexDevice); } }
+
+		/// <summary>
+		/// Loads the settings from XML.
+		/// </summary>
+		/// <param name="xml"></param>
+		/// <returns></returns>
+		[PublicAPI, XmlFactoryMethod(FACTORY_NAME)]
+		public static GlobalCacheFlexDeviceSettings FromXml(string xml)
+		{
+			GlobalCacheFlexDeviceSettings output = new GlobalCacheFlexDeviceSettings();
+			ParseXml(output, xml);
+			return output;
+		}
+	}
+}
