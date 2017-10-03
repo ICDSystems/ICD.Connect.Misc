9db045721bbf78329891fc82eeb3fa077c1a7798
Chris Cameron <chris.cameron@wedoresi.com>
2017-10-02 20:55:46 -0400

    Initial commit of GdITachFlexComPort and settings

---
+++ ICD.Connect.Misc.GlobalCache/Ports/GdITachFlexComPort.cs
@@ -0,0 +1,19 @@
+﻿using ICD.Connect.Protocol.Ports.ComPort;
+
+namespace ICD.Connect.Misc.GlobalCache.Ports
+{
+    public sealed class GdITachFlexComPort : AbstractComPort<GdITachFlexComPortSettings>
+    {
+		protected override bool SendFinal(string data)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override int SetComPortSpec(eComBaudRates baudRate, eComDataBits numberOfDataBits, eComParityType parityType,
+										   eComStopBits numberOfStopBits, eComProtocolType protocolType, eComHardwareHandshakeType hardwareHandShake,
+										   eComSoftwareHandshakeType softwareHandshake, bool reportCtsChanges)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ ICD.Connect.Misc.GlobalCache/Ports/GdITachFlexComPortSettings.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using ICD.Common.Properties;
+using ICD.Common.Utils.Xml;
+using ICD.Connect.Protocol.Ports.ComPort;
+using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Misc.GlobalCache.Ports {
+	public sealed class GdITachFlexComPortSettings : AbstractComPortSettings
+	{
+		private const string FACTORY_NAME = "GcITachComPort";
+
+		private const string PARENT_DEVICE_ELEMENT = "Device";
+
+		#region Properties
+
+		[SettingsProperty(SettingsProperty.ePropertyType.DeviceId)]
+		public int? Device { get; set; }
+
+		/// <summary>
+		/// Gets the originator factory name.
+		/// </summary>
+		public override string FactoryName { get { return FACTORY_NAME; } }
+
+		/// <summary>
+		/// Gets the type of the originator for this settings instance.
+		/// </summary>
+		public override Type OriginatorType { get { return typeof(GdITachFlexComPort); } }
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Writes property elements to xml.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void WriteElements(IcdXmlTextWriter writer)
+		{
+			base.WriteElements(writer);
+
+			writer.WriteElementString(PARENT_DEVICE_ELEMENT, IcdXmlConvert.ToString(Device));
+		}
+
+		/// <summary>
+		/// Returns the collection of ids that the settings will depend on.
+		/// For example, to instantiate an IR Port from settings, the device the physical port
+		/// belongs to will need to be instantiated first.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<int> GetDeviceDependencies()
+		{
+			if (Device != null)
+				yield return (int)Device;
+		}
+
+		/// <summary>
+		/// Loads the settings from XML.
+		/// </summary>
+		/// <param name="xml"></param>
+		/// <returns></returns>
+		[PublicAPI, XmlFactoryMethod(FACTORY_NAME)]
+		public static GdITachFlexComPortSettings FromXml(string xml)
+		{
+			int? device = XmlUtils.TryReadChildElementContentAsInt(xml, PARENT_DEVICE_ELEMENT);
+
+			GdITachFlexComPortSettings output = new GdITachFlexComPortSettings
+			{
+				Device = device
+			};
+
+			ParseXml(output, xml);
+			return output;
+		}
+
+		#endregion
+	}
+}
