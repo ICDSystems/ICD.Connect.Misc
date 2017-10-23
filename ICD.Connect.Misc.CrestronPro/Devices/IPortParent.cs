ccdc9cc9f9ddd06ed3aa421b39f0f0a9af52b949
Chris Cameron <chris.cameron@wedoresi.com>
2017-10-23 05:41:46 -0400

    IPortParent interface is available in net standard

--- ICD.Connect.Misc.CrestronPro/Devices/IPortParent.cs
+++ ICD.Connect.Misc.CrestronPro/Devices/IPortParent.cs
@@ -1,5 +1,6 @@
 ﻿#if SIMPLSHARP
 using Crestron.SimplSharpPro;
+#endif
 using ICD.Common.Properties;
 
 namespace ICD.Connect.Misc.CrestronPro.Devices
@@ -10,6 +11,7 @@ namespace ICD.Connect.Misc.CrestronPro.Devices
 	[PublicAPI]
 	public interface IPortParent
 	{
+#if SIMPLSHARP
 		/// <summary>
 		/// Gets the port at the given address.
 		/// </summary>
@@ -41,6 +43,6 @@ namespace ICD.Connect.Misc.CrestronPro.Devices
 		/// <returns></returns>
 		[PublicAPI]
 		Versiport GetIoPort(int address);
+#endif
 	}
 }
-#endif
