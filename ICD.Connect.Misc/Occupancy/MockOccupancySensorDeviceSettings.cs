4cbd4eed07f38768eac1fd7ac73b79b60528285f
Chris Cameron <chris.cameron@wedoresi.com>
2018-02-09 09:52:12 -0500

    Removing xml factory method

--- ICD.Connect.Misc/Occupancy/MockOccupancySensorDeviceSettings.cs
+++ ICD.Connect.Misc/Occupancy/MockOccupancySensorDeviceSettings.cs
@@ -1,33 +1,16 @@
 ﻿using System;
-using ICD.Common.Properties;
 using ICD.Connect.Devices;
 using ICD.Connect.Settings.Attributes;
 
 namespace ICD.Connect.Misc.Occupancy
 {
+	[KrangSettings(FACTORY_NAME)]
 	public sealed class MockOccupancySensorDeviceSettings : AbstractDeviceSettings
 	{
-
 		private const string FACTORY_NAME = "MockOccupancySensorDevice";
-
 
 		public override string FactoryName { get { return FACTORY_NAME; } }
 
-
 		public override Type OriginatorType { get { return typeof(MockOccupancySensorDevice); } }
-
-		/// <summary>
-		/// Loads the settings from XML.
-		/// </summary>
-		/// <param name="xml"></param>
-		/// <returns></returns>
-		[PublicAPI, XmlFactoryMethod(FACTORY_NAME)]
-		public static MockOccupancySensorDeviceSettings FromXml(string xml)
-		{
-			MockOccupancySensorDeviceSettings output = new MockOccupancySensorDeviceSettings();
-			
-			ParseXml(output, xml);
-			return output;
-		}
 	}
 }
