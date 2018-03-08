97bbb04bcb2847c921b922b181863c37b89f2135
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-08 10:49:28 -0500

    Tidying

--- ICD.Connect.TvPresets/Properties/AssemblyInfo.cs
+++ ICD.Connect.TvPresets/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyTitle("ICD.Connect.TvPresets")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.TvPresets")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2017")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
 [assembly: AssemblyVersion("2.0.0.0")]
