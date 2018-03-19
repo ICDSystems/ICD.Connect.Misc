11c1996aca62ed5778e42dabf61246e8864bc452
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-03-19 11:21:14 -0400

    Add CresnetSettingsUtils

---
+++ ICD.Connect.Misc.CrestronPro/Utils/CresnetSettingsUtils.cs
@@ -0,0 +1,9 @@
+namespace ICD.Connect.Misc.CrestronPro.Utils
+{
+	public static class CresnetSettingsUtils
+	{
+		public const string CRESNET_ID_ELEMENT = "CresnetID";
+		public const string BRANCH_ID_ELEMENT = "BranchID";
+		public const string PARENT_ID_ELEMENT = "ParentID";
+	}
+}
