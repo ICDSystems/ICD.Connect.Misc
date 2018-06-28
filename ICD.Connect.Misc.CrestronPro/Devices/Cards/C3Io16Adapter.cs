17fa7f6ff29153e5ae4869880fefa03e4fe0b4ed
Chris Cameron <chris.cameron@wedoresi.com>
2018-06-28 17:13:35 -0400

    fix: Adding missing GetIoPort implementation to C3Io16Adapter

--- ICD.Connect.Misc.CrestronPro/Devices/Cards/C3Io16Adapter.cs
+++ ICD.Connect.Misc.CrestronPro/Devices/Cards/C3Io16Adapter.cs
@@ -1,4 +1,6 @@
-﻿#if SIMPLSHARP
+﻿using System;
+#if SIMPLSHARP
+using Crestron.SimplSharpPro;
 using Crestron.SimplSharpPro.ThreeSeriesCards;
 using Crestron.SimplSharpProInternal;
 #endif
@@ -8,6 +10,20 @@ namespace ICD.Connect.Misc.CrestronPro.Devices.Cards
 #if SIMPLSHARP
 	public sealed class C3Io16Adapter : AbstractC3CardAdapter<C3io16, C3Io16AdapterSettings>
 	{
+		/// <summary>
+		/// Gets the port at the given address.
+		/// </summary>
+		/// <param name="address"></param>
+		/// <returns></returns>
+		public override Versiport GetIoPort(int address)
+		{
+			if (Card.VersiPorts.Contains((uint)address))
+				return Card.VersiPorts[(uint)address];
+
+			string message = string.Format("{0} has no {1} with address {2}", this, typeof(Versiport).Name, address);
+			throw new IndexOutOfRangeException(message);
+		}
+
 		/// <summary>
 		/// Instantiates the card for the given card frame parent.
 		/// </summary>
