3786b1409f8502fd6b6c14e4edde49649bb7645c
Chris Cameron <chris.cameron@wedoresi.com>
2019-04-19 16:25:08 -0400

    chore: Updating changelog, incrementing minor version

--- ICD.Connect.Misc.CrestronPro/Properties/AssemblyInfo.cs
+++ ICD.Connect.Misc.CrestronPro/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Misc.CrestronPro")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Misc.CrestronPro")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("6.0.2.0")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyVersion("6.1.0.0")]
--- ICD.Connect.Misc.GlobalCache/Properties/AssemblyInfo.cs
+++ ICD.Connect.Misc.GlobalCache/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Misc.GlobalCache")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Misc.GlobalCache")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("6.0.2.0")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyVersion("6.1.0.0")]
