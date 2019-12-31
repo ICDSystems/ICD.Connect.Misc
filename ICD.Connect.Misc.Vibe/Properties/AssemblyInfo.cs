d5bdfbe283a54d0f1e3bd9f35b72c08db1c2a0f5
Jeffery Thompson <jeffery.thompson@icdsystems.com>
2019-12-31 14:44:07 -0500

    chore: s# csproj

---
+++ ICD.Connect.Misc.Vibe/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("ICD.Connect.Misc.Vibe")]
+[assembly: AssemblyCompany("ICD Systems")]
+[assembly: AssemblyProduct("ICD.Connect.Misc.Vibe")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyVersion("8.0.0.0")]
