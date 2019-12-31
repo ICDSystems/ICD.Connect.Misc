6c3124c28fb3d082ee19570644e338eaea651c1f
Laura Gomez <laura.gomez@profound-tech.com>
2019-12-31 14:16:17 -0500

    refactor : fixing error converted and response.

--- ICD.Connect.Misc.Unsplash/Converters/AbstractUnsplashResponseConverter.cs
+++ ICD.Connect.Misc.Unsplash/Converters/AbstractUnsplashResponseConverter.cs
@@ -1,20 +1,25 @@
-﻿using ICD.Common.Utils.Json;
+﻿using ICD.Common.Utils.Extensions;
+using ICD.Common.Utils.Json;
 using ICD.Connect.Misc.Unsplash_NetStandard.Responses;
 using Newtonsoft.Json;
 
 namespace ICD.Connect.Misc.Unsplash_NetStandard.Converters
 {
-	public abstract class UnsplashAbstractResponseConverter<T> : AbstractGenericJsonConverter<T>
-		where T : UnsplashAbstractResponse
+	public abstract class AbstractUnsplashResponseConverter<T> : AbstractGenericJsonConverter<T>
+		where T : AbstractUnsplashResponse
 	{
 		private const string ATTRIBUTE_ERROR = "error";
+		private const string ATTRIBUTE_ERROR_DESCRIPTION = "error_description";
 
 		protected override void ReadProperty(string property, JsonReader reader, T instance, JsonSerializer serializer)
 		{
 			switch (property)
 			{
 				case ATTRIBUTE_ERROR:
-					instance.Error = serializer.Deserialize<Error>(reader);
+					instance.Error = reader.GetValueAsString();
+					break;
+				case ATTRIBUTE_ERROR_DESCRIPTION:
+					instance.ErrorDescription = reader.GetValueAsString();
 					break;
 				default:
 					base.ReadProperty(property, reader, instance, serializer);
---
+++ ICD.Connect.Misc.Unsplash/Responses/AbstractUnsplashResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ICD.Connect.Misc.Unsplash_NetStandard.Responses
+{
+	public abstract class AbstractUnsplashResponse
+	{
+		public string Error { get; set; }
+		public string ErrorDescription { get; set; }
+	}
+}
