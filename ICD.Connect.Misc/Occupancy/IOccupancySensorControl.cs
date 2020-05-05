65a775e22cd73ba8f9ee209d7fa03ae95cad8747
Chris Cameron <chris.cameron@wedoresi.com>
2020-05-05 10:08:42 -0400

    refactor: Simplified property telemetry

--- ICD.Connect.Misc/Occupancy/IOccupancySensorControl.cs
+++ ICD.Connect.Misc/Occupancy/IOccupancySensorControl.cs
@@ -27,7 +27,7 @@ namespace ICD.Connect.Misc.Occupancy
 		/// True = occupied
 		/// False = unoccupied/vacant
 		/// </summary>
-		[DynamicPropertyTelemetry(OccupancyTelemetryNames.OCCUPANCY_STATE, null, OccupancyTelemetryNames.OCCUPANCY_STATE_CHANGED)]
+		[PropertyTelemetry(OccupancyTelemetryNames.OCCUPANCY_STATE, null, OccupancyTelemetryNames.OCCUPANCY_STATE_CHANGED)]
 		eOccupancyState OccupancyState { get; }
 	}
 }
