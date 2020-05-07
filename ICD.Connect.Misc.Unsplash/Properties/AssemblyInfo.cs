f6de929b833a6ab675cac07dbcea6c883b475252
Chris Cameron <chris.cameron@wedoresi.com>
2020-05-07 11:04:19 -0400

    chore: Adding AssemblyInfo to Unsplash project

---
+++ ICD.Connect.Misc.Unsplash/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("ICD.Connect.Misc.Unsplash")]
+[assembly: AssemblyCompany("ICD Systems")]
+[assembly: AssemblyProduct("ICD.Connect.Misc.Unsplash")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyVersion("8.3.0.0")]
