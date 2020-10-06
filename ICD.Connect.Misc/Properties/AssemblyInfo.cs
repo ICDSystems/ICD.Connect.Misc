8aa9a3606e9c6c16608e8b663364858b27501fee
Chris Cameron <chris.cameron@wedoresi.com>
2020-10-06 18:45:16 -0400

    chore: Updating changelog, incrementing minor version

--- ICD.Connect.Misc/Properties/AssemblyInfo.cs
+++ ICD.Connect.Misc/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Misc")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
-[assembly: AssemblyVersion("8.3.2.0")]
+[assembly: AssemblyVersion("8.4.0.0")]
