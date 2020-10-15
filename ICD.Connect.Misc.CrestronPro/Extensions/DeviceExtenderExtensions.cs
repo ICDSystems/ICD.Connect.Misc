296ef5710a804f45435bdace8b853b80267a6b4a
Chris Cameron <chris.cameron@wedoresi.com>
2020-10-15 16:11:13 -0400

    feat: Added extension method for getting DeviceExtender sigs

---
+++ ICD.Connect.Misc.CrestronPro/Extensions/DeviceExtenderExtensions.cs
@@ -0,0 +1,36 @@
+﻿#if SIMPLSHARP
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Crestron.SimplSharp.Reflection;
+using Crestron.SimplSharpPro;
+using ICD.Common.Properties;
+
+namespace ICD.Connect.Misc.CrestronPro.Extensions
+{
+	public static class DeviceExtenderExtensions
+	{
+		/// <summary>
+		/// Gets the available output sigs for the extender.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <returns></returns>
+		public static IEnumerable<Sig> GetOutputSigs([NotNull] this DeviceExtender extends)
+		{
+			if (extends == null)
+				throw new ArgumentNullException("extends");
+
+			return extends.GetType()
+			              .GetCType()
+			              .GetProperties(BindingFlags.Public |
+			                             BindingFlags.Instance |
+			                             BindingFlags.FlattenHierarchy)
+			              .Where(p => typeof(StringOutputSig).IsAssignableFrom(p.PropertyType) ||
+			                          typeof(StringOutputSig).IsAssignableFrom(p.PropertyType) ||
+			                          typeof(StringOutputSig).IsAssignableFrom(p.PropertyType))
+			              .Select(p => p.GetValue(extends, null))
+			              .OfType<Sig>();
+		}
+	}
+}
+#endif
