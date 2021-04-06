408df9cd5e5efa4f292baa2139c3ecc12d34abb9
Chris Cameron <chris.cameron@wedoresi.com>
2021-04-06 11:40:11 -0400

    feat: Adding mock routing control to WindowsControlSystem

--- ICD.Connect.Misc.Windows/Devices/ControlSystems/WindowsControlSystem.cs
+++ ICD.Connect.Misc.Windows/Devices/ControlSystems/WindowsControlSystem.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ICD.Connect.API.Commands;
+using ICD.Connect.Devices.Controls;
 using ICD.Connect.Misc.ControlSystems;
 #if !SIMPLSHARP
 using ICD.Connect.Misc.Windows.Utils;
 #endif
+using ICD.Connect.Settings;
 
 namespace ICD.Connect.Misc.Windows.Devices.ControlSystems
 {
@@ -24,6 +27,20 @@ namespace ICD.Connect.Misc.Windows.Devices.ControlSystems
 		protected override bool GetIsOnlineStatus()
 		{
 			return true;
+		}
+
+		/// <summary>
+		/// Override to add controls to the device.
+		/// </summary>
+		/// <param name="settings"></param>
+		/// <param name="factory"></param>
+		/// <param name="addControl"></param>
+		protected override void AddControls(WindowsControlSystemSettings settings, IDeviceFactory factory,
+											Action<IDeviceControl> addControl)
+		{
+			base.AddControls(settings, factory, addControl);
+
+			addControl(new WindowsControlSystemRoutingControl(this, 0));
 		}
 
 		#region Console
---
+++ ICD.Connect.Misc.Windows/Devices/ControlSystems/WindowsControlSystemRoutingControl.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ICD.Common.Utils.Extensions;
+using ICD.Common.Utils.Services;
+using ICD.Connect.Routing;
+using ICD.Connect.Routing.Connections;
+using ICD.Connect.Routing.Controls;
+using ICD.Connect.Routing.EventArguments;
+using ICD.Connect.Routing.RoutingGraphs;
+
+namespace ICD.Connect.Misc.Windows.Devices.ControlSystems
+{
+	public sealed class WindowsControlSystemRoutingControl : AbstractRouteDestinationControl<WindowsControlSystem>,
+	                                                         IRouteSourceControl
+	{
+		/// <summary>
+		/// Raised when an input source status changes.
+		/// </summary>
+		public override event EventHandler<SourceDetectionStateChangeEventArgs> OnSourceDetectionStateChange;
+
+		/// <summary>
+		/// Raised when the device starts/stops actively using an input, e.g. unroutes an input.
+		/// </summary>
+		public override event EventHandler<ActiveInputStateChangeEventArgs> OnActiveInputsChanged;
+
+		/// <summary>
+		/// Raised when the device starts/stops actively transmitting on an output.
+		/// </summary>
+		public event EventHandler<TransmissionStateEventArgs> OnActiveTransmissionStateChanged;
+
+		private IRoutingGraph m_CachedRoutingGraph;
+
+		/// <summary>
+		/// Gets the routing graph.
+		/// </summary>
+		public IRoutingGraph RoutingGraph
+		{
+			get { return m_CachedRoutingGraph = m_CachedRoutingGraph ?? ServiceProvider.GetService<IRoutingGraph>(); }
+		}
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="parent"></param>
+		/// <param name="id"></param>
+		public WindowsControlSystemRoutingControl(WindowsControlSystem parent, int id)
+			: base(parent, id)
+		{
+		}
+
+		/// <summary>
+		/// Override to release resources.
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected override void DisposeFinal(bool disposing)
+		{
+			OnSourceDetectionStateChange = null;
+			OnActiveInputsChanged = null;
+			OnActiveTransmissionStateChanged = null;
+
+			base.DisposeFinal(disposing);
+		}
+
+		/// <summary>
+		/// Returns true if a signal is detected at the given input.
+		/// </summary>
+		/// <param name="input"></param>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		public override bool GetSignalDetectedState(int input, eConnectionType type)
+		{
+			return true;
+		}
+
+		/// <summary>
+		/// Returns the true if the input is actively being used by the source device.
+		/// For example, a display might true if the input is currently on screen,
+		/// while a switcher may return true if the input is currently routed.
+		/// </summary>
+		public override bool GetInputActiveState(int input, eConnectionType type)
+		{
+			return true;
+		}
+
+		/// <summary>
+		/// Gets the input at the given address.
+		/// </summary>
+		/// <param name="input"></param>
+		/// <returns></returns>
+		public override ConnectorInfo GetInput(int input)
+		{
+			if (!ContainsInput(input))
+				throw new ArgumentOutOfRangeException("input");
+
+			return new ConnectorInfo(input, eConnectionType.Audio);
+		}
+
+		/// <summary>
+		/// Returns true if the destination contains an input at the given address.
+		/// </summary>
+		/// <param name="input"></param>
+		/// <returns></returns>
+		public override bool ContainsInput(int input)
+		{
+			Connection connection = RoutingGraph.Connections.GetInputConnection(this, input);
+			return connection != null && connection.ConnectionType.HasFlag(eConnectionType.Audio);
+		}
+
+		/// <summary>
+		/// Returns true if the device is actively transmitting on the given output.
+		/// This is NOT the same as sending video, since some devices may send an
+		/// idle signal by default.
+		/// </summary>
+		/// <param name="output"></param>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		public bool GetActiveTransmissionState(int output, eConnectionType type)
+		{
+			return true;
+		}
+
+		/// <summary>
+		/// Gets the output at the given address.
+		/// </summary>
+		/// <param name="output"></param>
+		/// <returns></returns>
+		public ConnectorInfo GetOutput(int output)
+		{
+			if (!ContainsOutput(output))
+				throw new ArgumentOutOfRangeException("output");
+
+			return new ConnectorInfo(output, eConnectionType.Audio);
+		}
+
+		/// <summary>
+		/// Returns true if the source contains an output at the given address.
+		/// </summary>
+		/// <param name="output"></param>
+		/// <returns></returns>
+		public bool ContainsOutput(int output)
+		{
+			Connection connection = RoutingGraph.Connections.GetOutputConnection(this, output);
+			return connection != null && connection.ConnectionType.HasFlag(eConnectionType.Audio);
+		}
+
+		/// <summary>
+		/// Returns the inputs.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<ConnectorInfo> GetInputs()
+		{
+			return
+				RoutingGraph.Connections
+				            .GetInputConnections(Parent.Id, Id, eConnectionType.Audio)
+				            .Select(c => new ConnectorInfo(c.Destination.Address, eConnectionType.Audio));
+		}
+
+		/// <summary>
+		/// Returns the outputs.
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<ConnectorInfo> GetOutputs()
+		{
+			return
+				RoutingGraph.Connections
+				            .GetOutputConnections(Parent.Id, Id, eConnectionType.Audio)
+				            .Select(c => new ConnectorInfo(c.Source.Address, eConnectionType.Audio));
+		}
+	}
+}
