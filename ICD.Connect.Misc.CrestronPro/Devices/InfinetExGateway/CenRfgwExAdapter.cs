73efeb6e75b1bb51bb8cf3377d69b0f9cad88097
Drew Tingen <drew.tingen@icdsystems.com>
2021-07-10 01:22:28 -0400

    feat: Added CenRfgwEx Adapter

---
+++ ICD.Connect.Misc.CrestronPro/Devices/InfinetExGateway/CenRfgwExAdapter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using ICD.Common.Utils.Services.Logging;
+using ICD.Connect.Devices;
+using ICD.Connect.Settings;
+#if SIMPLSHARP
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.Gateways;
+#endif
+
+namespace ICD.Connect.Misc.CrestronPro.Devices.InfinetExGateway
+{
+	public sealed class CenRfgwExAdapter : AbstractDevice<CenRfgwExAdapterSettings>, IInfinetExGatewayAdapter
+	{
+
+#if SIMPLSHARP
+		private CenRfgwExEthernetSharable m_Gateway;
+
+		public GatewayBase InfinetExGateway { get { return m_Gateway; } }
+#endif
+
+		/// <summary>
+		/// Gets the current online status of the device.
+		/// </summary>
+		/// <returns></returns>
+		protected override bool GetIsOnlineStatus()
+		{
+#if SIMPLSHARP
+			return m_Gateway != null && m_Gateway.IsOnline;
+#else
+			return false;
+#endif
+		}
+
+#if SIMPLSHARP
+		protected override void CopySettingsFinal(CenRfgwExAdapterSettings settings)
+		{
+			base.CopySettingsFinal(settings);
+
+			settings.Ipid = (byte?)(m_Gateway == null ? 0 : m_Gateway.ID);
+		}
+
+		protected override void ClearSettingsFinal()
+		{
+			base.ClearSettingsFinal();
+
+			SetGateway(null);
+		}
+#endif
+
+		protected override void ApplySettingsFinal(CenRfgwExAdapterSettings settings, IDeviceFactory factory)
+		{
+			base.ApplySettingsFinal(settings, factory);
+
+#if SIMPLSHARP
+			CenRfgwExEthernetSharable gateway = null;
+
+			try
+			{
+				if (settings.Ipid.HasValue)
+					gateway = new CenRfgwExEthernetSharable(settings.Ipid.Value, ProgramInfo.ControlSystem);
+			}
+			catch (Exception e)
+			{
+				Logger.Log(eSeverity.Error, "Failed to instantiate {0} - {1}", typeof(CenRfgwExEthernetSharable).Name, e.Message);
+			}
+
+			SetGateway(gateway);
+#else
+			throw new NotSupportedException();
+#endif
+		}
+
+		#region Gateway Callbacks
+
+#if SIMPLSHARP
+
+		/// <summary>
+		/// Override to control how the switcher is assigned from settings.
+		/// </summary>
+		private void SetGateway(CenRfgwExEthernetSharable gateway)
+		{
+			Unsubscribe(m_Gateway);
+			m_Gateway = gateway;
+			Subscribe(m_Gateway);
+
+			UpdateCachedOnlineStatus();
+		}
+
+		private void Subscribe(CenRfgwExEthernetSharable gateway)
+		{
+			if (gateway == null)
+				return;
+
+			gateway.OnlineStatusChange += GatewayOnLineStatusChange;
+		}
+
+		private void Unsubscribe(CenRfgwExEthernetSharable gateway)
+		{
+			if (gateway == null)
+				return;
+
+			gateway.OnlineStatusChange -= GatewayOnLineStatusChange;
+		}
+
+		private void GatewayOnLineStatusChange(GenericBase currentDevice, OnlineOfflineEventArgs args)
+		{
+			UpdateCachedOnlineStatus();
+		}
+
+#endif
+
+		#endregion
+
+
+	}
+}
---
+++ ICD.Connect.Misc.CrestronPro/Devices/InfinetExGateway/CenRfgwExAdapterSettings.cs
@@ -0,0 +1,39 @@
+﻿using ICD.Common.Utils;
+using ICD.Common.Utils.Xml;
+using ICD.Connect.Devices;
+using ICD.Connect.Settings.Attributes;
+using ICD.Connect.Settings.Attributes.SettingsProperties;
+
+namespace ICD.Connect.Misc.CrestronPro.Devices.InfinetExGateway
+{
+	[KrangSettings("CenRfgwExAdapter", typeof(CenRfgwExAdapter))]
+	public sealed class CenRfgwExAdapterSettings : AbstractDeviceSettings
+	{
+		private const string IPID_ELEMENT = "IPID";
+
+		[CrestronByteSettingsProperty]
+		public byte? Ipid { get; set; }
+
+		/// <summary>
+		/// Writes property elements to xml.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void WriteElements(IcdXmlTextWriter writer)
+		{
+			base.WriteElements(writer);
+
+			writer.WriteElementString(IPID_ELEMENT, Ipid == null ? null : StringUtils.ToIpIdString(Ipid.Value));
+		}
+
+		/// <summary>
+		/// Updates the settings from xml.
+		/// </summary>
+		/// <param name="xml"></param>
+		public override void ParseXml(string xml)
+		{
+			base.ParseXml(xml);
+
+			Ipid = XmlUtils.TryReadChildElementContentAsByte(xml, IPID_ELEMENT) ?? 0;
+		}
+	}
+}
