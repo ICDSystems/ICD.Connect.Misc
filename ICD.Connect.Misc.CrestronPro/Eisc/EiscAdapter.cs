465d9fe27fbda2c0ba9cdb108bcef32311f62664
Drew Tingen <drew.tingen@icdsystems.com>
2022-05-21 23:31:24 -0400

    feat: Adding EiscAdapter

---
+++ ICD.Connect.Misc.CrestronPro/Eisc/EiscAdapter.cs
@@ -0,0 +1,260 @@
+﻿using ICD.Connect.Panels.Devices;
+using ICD.Connect.Panels.SigCollections;
+using ICD.Connect.Settings;
+#if !NETSTANDARD
+using ICD.Common.Utils.Services.Logging;
+using ICD.Connect.Misc.CrestronPro.Extensions;
+using ICD.Connect.Misc.CrestronPro.Utils;
+using ICD.Connect.Protocol.Sigs;
+using ICD.Connect.Misc.CrestronPro.Sigs;
+using Crestron.SimplSharpPro.EthernetCommunication;
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.DeviceSupport;
+#else
+using System;
+#endif
+
+namespace ICD.Connect.Misc.CrestronPro.Eisc
+{
+	public sealed class EiscAdapter : AbstractSigDevice<EiscAdapterSettings>
+	{
+		public byte EiscIpid { get; private set; }
+
+		public string EiscAddress { get; private set; }
+
+#if !NETSTANDARD
+		private EthernetIntersystemCommunications m_Eisc;
+
+		private readonly DeviceBooleanInputCollectionAdapter m_BooleanInput;
+		private readonly DeviceUShortInputCollectionAdapter m_UShortInput;
+		private readonly DeviceStringInputCollectionAdapter m_StringInput;
+		private readonly DeviceBooleanOutputCollectionAdapter m_BooleanOutput;
+		private readonly DeviceUShortOutputCollectionAdapter m_UShortOutput;
+		private readonly DeviceStringOutputCollectionAdapter m_StringOutput;
+#endif
+
+
+		public EiscAdapter()
+		{
+#if !NETSTANDARD
+
+			m_BooleanInput = new DeviceBooleanInputCollectionAdapter();
+			m_UShortInput = new DeviceUShortInputCollectionAdapter();
+			m_StringInput = new DeviceStringInputCollectionAdapter();
+			m_BooleanOutput = new DeviceBooleanOutputCollectionAdapter();
+			m_UShortOutput = new DeviceUShortOutputCollectionAdapter();
+			m_StringOutput = new DeviceStringOutputCollectionAdapter();
+
+#endif
+		}
+
+		/// <summary>
+		/// Gets the current online status of the device.
+		/// </summary>
+		/// <returns></returns>
+		protected override bool GetIsOnlineStatus()
+		{
+#if !NETSTANDARD
+			return m_Eisc != null && m_Eisc.IsOnline;
+#else
+			return false;
+#endif
+		}
+
+		/// <summary>
+		/// Collection of Boolean Inputs sent to the panel.
+		/// </summary>
+		public override IDeviceBooleanInputCollection BooleanInput
+		{
+			get
+			{
+#if !NETSTANDARD
+				return m_BooleanInput;
+#else
+				throw new NotSupportedException();
+#endif
+			}
+		}
+
+		/// <summary>
+		/// Collection of Integer Inputs sent to the panel.
+		/// </summary>
+		public override IDeviceUShortInputCollection UShortInput
+		{
+			get
+			{
+#if !NETSTANDARD
+				return m_UShortInput;
+#else
+				throw new NotSupportedException();
+#endif
+			}
+		}
+
+		/// <summary>
+		/// Collection of String Inputs sent to the panel.
+		/// </summary>
+		public override IDeviceStringInputCollection StringInput
+		{
+			get
+			{
+#if !NETSTANDARD
+				return m_StringInput;
+#else
+				throw new NotSupportedException();
+#endif
+			}
+		}
+
+		/// <summary>
+		/// Collection of Boolean Outputs sent from the panel.
+		/// </summary>
+		public override IDeviceBooleanOutputCollection BooleanOutput
+		{
+			get
+			{
+#if !NETSTANDARD
+				return m_BooleanOutput;
+#else
+				throw new NotSupportedException();
+#endif
+			}
+		}
+
+		/// <summary>
+		/// Collection of Integer Outputs sent from the panel.
+		/// </summary>
+		public override IDeviceUShortOutputCollection UShortOutput
+		{
+			get
+			{
+#if !NETSTANDARD
+				return m_UShortOutput;
+#else
+				throw new NotSupportedException();
+#endif
+			}
+		}
+
+		/// <summary>
+		/// Collection of String Outputs sent from the panel.
+		/// </summary>
+		public override IDeviceStringOutputCollection StringOutput
+		{
+			get
+			{
+#if !NETSTANDARD
+				return m_StringOutput;
+#else
+				throw new NotSupportedException();
+#endif
+			}
+		}
+
+#if !NETSTANDARD
+		private void SetEisc(EthernetIntersystemCommunications eisc)
+		{
+			if (m_Eisc != null)
+			{
+				Unsubscribe(m_Eisc);
+				eDeviceRegistrationUnRegistrationResponse tearDownResult;
+				if (GenericBaseUtils.TearDown(m_Eisc, out tearDownResult))
+					Logger.Log(eSeverity.Error, "Unable to unregister {0} - {1}", m_Eisc.GetType().Name, tearDownResult);
+
+			}
+
+			m_Eisc = eisc;
+
+			if (m_Eisc != null)
+			{
+
+				eDeviceRegistrationUnRegistrationResponse setUpResult;
+				if (!GenericBaseUtils.SetUp(m_Eisc, this, out setUpResult))
+					Logger.Log(eSeverity.Error, "Unable to register {0} - {1}", m_Eisc.GetType().Name, setUpResult);
+			}
+
+			m_BooleanInput.SetCollection(m_Eisc == null ? null : m_Eisc.BooleanInput);
+			m_UShortInput.SetCollection(m_Eisc == null ? null : m_Eisc.UShortInput);
+			m_StringInput.SetCollection(m_Eisc == null ? null : m_Eisc.StringInput);
+			m_BooleanOutput.SetCollection(m_Eisc == null ? null : m_Eisc.BooleanOutput);
+			m_UShortOutput.SetCollection(m_Eisc == null ? null : m_Eisc.UShortOutput);
+			m_StringOutput.SetCollection(m_Eisc == null ? null : m_Eisc.StringOutput);
+
+			Subscribe(m_Eisc);
+
+			UpdateCachedOnlineStatus();
+
+		}
+
+		private void Subscribe(EthernetIntersystemCommunications eisc)
+		{
+			if (eisc == null)
+				return;
+
+			eisc.SigChange += EiscOnSigChange;
+			eisc.OnlineStatusChange += EiscOnlineStatusChange;
+
+		}
+
+		private void EiscOnSigChange(BasicTriList currentdevice, SigEventArgs args)
+		{
+			SigInfo sigInfo = args.Sig.ToSigInfo();
+			RaiseOutputSigChangeCallback(sigInfo);
+		}
+
+		private void EiscOnlineStatusChange(GenericBase currentdevice, OnlineOfflineEventArgs args)
+		{
+			UpdateCachedOnlineStatus();
+		}
+
+		private void Unsubscribe(EthernetIntersystemCommunications eisc)
+		{
+			if (eisc == null)
+				return;
+
+			eisc.SigChange -= EiscOnSigChange;
+			eisc.OnlineStatusChange -= EiscOnlineStatusChange;
+
+		}
+#endif
+
+		#region Settings
+
+		protected override void ApplySettingsFinal(EiscAdapterSettings settings, IDeviceFactory factory)
+		{
+
+			base.ApplySettingsFinal(settings, factory);
+
+			EiscIpid = settings.EiscIpid;
+			EiscAddress = settings.EiscAddress;
+#if !NETSTANDARD
+			SetEisc(new EthernetIntersystemCommunications(EiscIpid, EiscAddress, ProgramInfo.ControlSystem));
+#else
+			throw new NotSupportedException();
+#endif
+		}
+
+		protected override void CopySettingsFinal(EiscAdapterSettings settings)
+		{
+			base.CopySettingsFinal(settings);
+
+			settings.EiscIpid = EiscIpid;
+			settings.EiscAddress = EiscAddress;
+		}
+
+		protected override void ClearSettingsFinal()
+		{
+			base.ClearSettingsFinal();
+
+			EiscIpid = default(byte);
+			EiscAddress = null;
+
+#if !NETSTANDARD
+			SetEisc(null);
+#endif
+
+		}
+
+		#endregion
+	}
+}
---
+++ ICD.Connect.Misc.CrestronPro/Eisc/EiscAdapterSettings.cs
@@ -0,0 +1,44 @@
+﻿using ICD.Common.Utils;
+using ICD.Common.Utils.Xml;
+using ICD.Connect.Devices;
+using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Misc.CrestronPro.Eisc
+{
+	[KrangSettings("EiscAdapter", typeof(EiscAdapter))]
+	public sealed class EiscAdapterSettings : AbstractDeviceSettings
+	{
+		private const string ELEMENT_EISC_IPID = "IPID";
+		private const string ELEMENT_EISC_ADDRESS = "Address";
+
+		public byte EiscIpid { get; set; }
+
+		public string EiscAddress { get; set; }
+
+		/// <summary>
+		/// Writes property elements to xml.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void WriteElements(IcdXmlTextWriter writer)
+		{
+			base.WriteElements(writer);
+
+			writer.WriteElementString(ELEMENT_EISC_IPID, StringUtils.ToIpIdString(EiscIpid));
+			writer.WriteElementString(ELEMENT_EISC_ADDRESS, IcdXmlConvert.ToString(EiscAddress));
+		}
+
+		/// <summary>
+		/// Updates the settings from xml.
+		/// </summary>
+		/// <param name="xml"></param>
+		public override void ParseXml(string xml)
+		{
+			base.ParseXml(xml);
+
+			EiscIpid = XmlUtils.ReadChildElementContentAsByte(xml, ELEMENT_EISC_IPID);
+			EiscAddress = XmlUtils.ReadChildElementContentAsString(xml, ELEMENT_EISC_ADDRESS);
+
+
+		}
+	}
+}
